8928a39842dd358d71b60963a929677bc905d7b2
isa <isaemilia.h@gmail.com>
2019-12-04 10:45:02 +0200

    Add final touches.
    For now at least

--- script.js
+++ script.js
@@ -13,13 +13,13 @@ const container = document.getElementById('container')
 //functions
 function tip() {
     tipOutput.innerHTML = tipPercent.value + "%";
-    tipValue.innerHTML = (totalBill.value * (tipPercent.value / 100)).toFixed(2);
+    tipValue.innerHTML = "€ " +  (totalBill.value * (tipPercent.value / 100)).toFixed(2);
     console.log(tipPercent.value);
 }
 
 function splitInput() {
     people.innerHTML = split.value;
-    newBill.innerHTML = (totalBill.value / split.value).toFixed(2);
+    newBill.innerHTML = "€ " + (totalBill.value / split.value).toFixed(2);
     console.log(split.value);
 }
 
